25244131c6d353459bcae0ea90d232d00fdbccda
YHYAHIA <yhyahia@gmail.com>
2025-01-16 16:38:39 +0200

    67

--- dameg_area.gd
+++ dameg_area.gd
@@ -9,6 +9,7 @@ func _on_body_entered(body: Node2D) -> void:
 		if body.has_method("set_health"):  # Ensure the player has a health setter
 			body.set_health(body.get_health()-erea_damage)  # Apply damage
 			print("Player hit! Damage:", erea_damage)
+			GlobalInteract.plyer_geting_damge=true
 			pass
 				
 	 # Replace with function body.
@@ -19,4 +20,5 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_dameg_area_body_exited(body: Node2D) -> void:
+	GlobalInteract.plyer_geting_damge=false
 	pass # Replace with function body.
--- scene/fire.gd
+++ scene/fire.gd
@@ -9,7 +9,7 @@ func _on_body_entered(body: Node2D) -> void:
 		if body.has_method("set_health"):  # Ensure the player has a health setter
 			body.set_health(body.get_health()-erea_damage)  # Apply damage
 			print("Player hit! Damage:", erea_damage)
-			pass
+			GlobalInteract.plyer_geting_damge=true
 				
 	 # Replace with function body.
 
@@ -22,4 +22,5 @@ func _on_body_entered(body: Node2D) -> void:
 
 
 func _on_body_exited(body: Node2D) -> void:
+	GlobalInteract.plyer_geting_damge=false
 	pass # Replace with function body.
