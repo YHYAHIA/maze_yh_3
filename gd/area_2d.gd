26669c8a3917ef39cc9e042a6ff2ad7aad14ce98
YHYAHIA <yhyahia@gmail.com>
2025-01-16 10:54:48 +0200

    60

--- gd/area_2d.gd
+++ gd/area_2d.gd
@@ -8,10 +8,7 @@ func _on_body_entered(body: Node2D) -> void:
 		# Reduce the player's speed by the percentage
 		if body.has_method("set_speed"):
 			body.set_speed(body.speed * (1 - speed_modifier))
-		if player.dir.x < 0:
-			player.velocity.y +=player.speed /1.3
-		elif player.dir.x > 0:
-			player.velocity.y -= player.speed/1.3
+
 
 func _on_body_exited(body: Node2D) -> void:
 	if body == player:
