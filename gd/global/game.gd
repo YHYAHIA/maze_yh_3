1eaa9a15d364a6e2b32c6f29473881bbc42fb7d2
YHYAHIA <yhyahia@gmail.com>
2025-01-21 10:30:03 +0200

    90

--- gd/global/game.gd
+++ gd/global/game.gd
@@ -1,3 +1,4 @@
 extends Node
 
 var quest1acceoted :bool = false
+var boars_sline:int = 0
--- scene/quests.gd
+++ scene/quests.gd
@@ -5,4 +5,6 @@ extends Control
 
 func _process(delta: float) -> void:
 		if Game.quest1acceoted:
-			get_node("QUESTS").text = "boars sline:" str(Game.boars_sline)
+			get_node("QUESTS").text = "boars sline:" +str(Game.boars_sline)
+		else:
+			get_node("QUESTS").text = "no quest active at the moment !" 
