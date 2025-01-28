f50661298505025a0920bbaa4d1d33f13e933ecd
YHYAHIA <yhyahia@gmail.com>
2025-01-28 15:06:07 +0200

    143

--- gd/start.gd
+++ gd/start.gd
@@ -22,3 +22,7 @@ func _on_exit_pressed() -> void:
 func _on_levels_pressed() -> void:
 	get_tree().change_scene_to_file("res://scene/levels.tscn")
 	 # Replace with function body.
+
+
+func _on_continue_pressed() -> void:
+	pass # Replace with function body.
