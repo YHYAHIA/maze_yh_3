82f06f8d6cc36a897389705e8c8805348f3d53bb
YHYAHIA <yhyahia@gmail.com>
2025-02-03 10:45:12 +0200

    170

--- gd/exit.gd
+++ gd/exit.gd
@@ -1,11 +1,12 @@
 extends Area2D
 
 const file_start="res://scene/level_"
-
+@export var level_name : String = "Level2"
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		$"../AudioStreamPlayer2D".play()
+		SaveSystem.save_level(level_name)
 		var currnt_scene_file=get_tree().current_scene.scene_file_path
 		var next_level_numper=currnt_scene_file.to_int()+1
 		var next_level_path = file_start+str(next_level_numper)+".tscn"
@@ -14,3 +15,7 @@ func _on_body_entered(body: Node2D) -> void:
 		get_tree().change_scene_to_file(next_level_path)
 		#get_tree().change_scene_to_file(next_level_path)
 	 # Replace with function body.
+
+
+func _on_body_exited(body: Node2D) -> void:
+	pass # Replace with function body.
--- gd/next_level_3.gd
+++ gd/next_level_3.gd
@@ -6,6 +6,7 @@ const file_start="res://scene/level_"
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("player"):
 		$"../AudioStreamPlayer2D".play()
+		
 		var currnt_scene_file=get_tree().current_scene.scene_file_path
 		var next_level_numper=currnt_scene_file.to_int()+1
 		var next_level_path = file_start+str(next_level_numper)+".tscn"
